78b3d7c6aaaa6f2a0df9d97cd1034a41e0fa9936
Heather White <miss.pomykal@gmail.com>
2022-01-15 16:33:28 -0500

    MAP TEST: add navigation to App.js, index.js, and temporary link in LoginScreen.js to render PublicMap

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import "react-native-gesture-handler";
-import React, { useEffect, useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { LoginScreen, HomeScreen, RegistrationScreen } from "./src/screens";
-import { decode, encode } from "base-64";
+import 'react-native-gesture-handler'
+import React, { useEffect, useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import {
+  LoginScreen,
+  HomeScreen,
+  RegistrationScreen,
+  PublicMapScreen,
+} from './src/screens'
+import { decode, encode } from 'base-64'
 if (!global.btoa) {
-  global.btoa = encode;
+  global.btoa = encode
 }
 if (!global.atob) {
-  global.atob = decode;
+  global.atob = decode
 }
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
 
   return (
     <NavigationContainer>
@@ -28,11 +33,12 @@ export default function App() {
           <>
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Registration" component={RegistrationScreen} />
+            <Stack.Screen name="PublicMap" component={PublicMapScreen} />
           </>
         )}
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
 
 // const styles = StyleSheet.create({

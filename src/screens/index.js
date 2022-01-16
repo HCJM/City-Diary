c1029b99d89d3b5dc127bcac8ddf017fe2a82690
Heather White <miss.pomykal@gmail.com>
2022-01-15 22:30:31 -0500

    DRAWER TEST: add personal map screen nav, hook up nav,  delete unecessary header file

--- src/screens/index.js
+++ src/screens/index.js
@@ -4,4 +4,6 @@ export { default as HomeScreen } from './HomeScreen/HomeScreen'
 
 export { default as RegistrationScreen } from './RegistrationScreen/RegistrationScreen'
 
-export { default as PublicMapScreen } from './MapScreen/PublicMapScreen'
+export { default as PublicMapScreen } from './PublicMapScreen/PublicMapScreen'
+
+export { default as PersonalMapScreen } from './PersonalMapScreen/PersonalMapScreen'

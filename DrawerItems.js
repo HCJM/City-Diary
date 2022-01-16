3930a0e006514643c7260fb586bd84d38edf88d2
Heather White <miss.pomykal@gmail.com>
2022-01-15 22:02:26 -0500

    DRAWER TEST: install react-navigation/drawer, add to babel.config, use drawer nav instead of stack nav

---
+++ DrawerItems.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    name: 'Login',
+    //   iconType:'Material',
+    //   iconName:'face-profile'
+  },
+  {
+    name: 'Registration',
+    //   iconType:'Material',
+    //   iconName:'face-profile'
+  },
+  {
+    name: 'Home',
+    // iconType:'Feather',
+    // iconName:'settings'
+  },
+  {
+    name: 'Personal Audio Map',
+    // iconType:'Material',
+    // iconName:'bookmark-check-outline'
+  },
+  {
+    name: 'Public Audio Map',
+    // iconType:'FontAwesome5',
+    // iconName:'user-friends'
+  },
+]

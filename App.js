90af4a38b66f0ae9a29f9ac22f3b4deb07be598e
Heather White <miss.pomykal@gmail.com>
2022-01-15 22:02:26 -0500

    DRAWER TEST: install react-navigation/drawer, add to babel.config, use drawer nav instead of stack nav

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import 'react-native-gesture-handler'
 import React, { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import {
   LoginScreen,
   HomeScreen,
   RegistrationScreen,
   PublicMapScreen,
 } from './src/screens'
+import DrawerItems from './DrawerItems'
 import { decode, encode } from 'base-64'
 if (!global.btoa) {
   global.btoa = encode
@@ -17,6 +19,7 @@ if (!global.atob) {
 }
 
 const Stack = createStackNavigator()
+const Drawer = createDrawerNavigator()
 
 export default function App() {
   const [loading, setLoading] = useState(true)
@@ -24,7 +27,32 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Drawer.Navigator
+        drawerPosition="right"
+        drawerType="front"
+        initialRouteName="Home"
+        screenOptions={{
+          activeTintColor: '#e91e63',
+          itemStyle: { marginVertical: 10 },
+        }}
+      >
+        {DrawerItems.map((drawer) => (
+          <Drawer.Screen
+            key={drawer.name}
+            name={drawer.name}
+            component={
+              drawer.name === 'Login'
+                ? LoginScreen
+                : drawer.name === 'Registration'
+                ? RegistrationScreen
+                : drawer.name === 'Home'
+                ? HomeScreen
+                : PublicMapScreen
+            }
+          />
+        ))}
+      </Drawer.Navigator>
+      {/* <Stack.Navigator>
         {user ? (
           <Stack.Screen name="Home">
             {(props) => <HomeScreen {...props} extraData={user} />}
@@ -36,10 +64,38 @@ export default function App() {
             <Stack.Screen name="Public Audio Map" component={PublicMapScreen} />
           </>
         )}
-      </Stack.Navigator>
+      </Stack.Navigator> */}
     </NavigationContainer>
   )
 }
+
+/*
+<Drawer.Navigator
+        drawerType="front"
+        drawerPosition="right"
+        // initialRouteName="Home"
+        drawerContentOptions={{
+          activeTintColor: '#e91e63',
+          itemStyle: { marginVertical: 10 },
+        }}
+      >
+        {DrawerItems.map((drawer) => (
+          <Drawer.Screen
+            key={drawer.name}
+            name={drawer.name}
+            component={
+              drawer.name === 'Login'
+                ? LoginScreen
+                : drawer.name === 'Registration'
+                ? RegistrationScreen
+                : drawer.name === 'Home'
+                ? HomeScreen
+                : PublicMapScreen
+            }
+          />
+        ))}
+      </Drawer.Navigator>
+*/
 
 // const styles = StyleSheet.create({
 //   container: {

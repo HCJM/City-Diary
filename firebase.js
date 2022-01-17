0d7e82066f93b7ba959e4b56a67955f9c27d0497
Heather White <miss.pomykal@gmail.com>
2022-01-17 15:54:00 -0500

    SETUP: add import of firebase/compat/auth & ../firestore to firebase.js & add onRegisterPress logic to RegistrationScreen

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/compat/app'
-// import 'firebase/compat/auth'
-// import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
 
 //Initialize Firebase
 const firebaseConfig = {

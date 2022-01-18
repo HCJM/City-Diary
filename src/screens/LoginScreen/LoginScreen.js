2d56af3ad006029e2011d81a8a8b2df40c4bfad0
Heather White <miss.pomykal@gmail.com>
2022-01-18 15:36:10 -0500

    UPDATE: on login redirect to PublicMapScreen view

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -28,7 +28,7 @@ export default function LoginScreen({ navigation }) {
               return
             }
             const user = firestoreDocument.data()
-            navigation.navigate('Home', { user })
+            navigation.navigate('Public Audio Map', { user })
           })
           .catch((error) => {
             alert(error)

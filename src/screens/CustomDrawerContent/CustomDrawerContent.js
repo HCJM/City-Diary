b78e06280e78e6954d16dc6dea80f241847fcd25
mauilema <srjorgemaciel2002@gmail.com>
2022-01-19 23:54:05 -0500

    added styling to sign out route on drawer container

--- src/screens/CustomDrawerContent/CustomDrawerContent.js
+++ src/screens/CustomDrawerContent/CustomDrawerContent.js
@@ -1,16 +1,19 @@
 import * as React from 'react'
+import styles from "./styles"
 import { Image, Text, View, TouchableOpacity } from "react-native"
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer'
 
 export function CustomDrawerContent(props) {
     return (
-      
-      <DrawerContentScrollView {...props}>
+      <>
+        <DrawerContentScrollView {...props}>
         <DrawerItemList {...props}/>
-        <DrawerItem 
-        label='Sign Out'
-        onPress={() => console.log('sign out functionality')}
-        />
-      </DrawerContentScrollView>
+          <DrawerItem 
+          style={styles.signOutItem}
+          label='Sign Out'
+          onPress={() => console.log('sign out functionality')}
+          />
+        </DrawerContentScrollView>
+      </>
     )
   }

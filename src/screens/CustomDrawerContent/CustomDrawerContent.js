50c711509500c512c644a87504b26d3ce1253836
mauilema <srjorgemaciel2002@gmail.com>
2022-01-20 17:20:38 -0500

    testing sign out functionality

--- src/screens/CustomDrawerContent/CustomDrawerContent.js
+++ src/screens/CustomDrawerContent/CustomDrawerContent.js
@@ -2,38 +2,39 @@ import * as React from 'react'
 import styles from "./styles"
 import { Image, Text, View, TouchableOpacity } from "react-native"
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer'
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth'
-import { auth } from '../../../firebase'
-
-
-
-
+// import firebase from 'firebase/compat/app'
+// import 'firebase/compat/auth'
+import { firebase } from '../../../firebase.js'
+ 
+ 
+ 
+ 
+ 
 export function CustomDrawerContent(props) {
-
-  const signedInUser = auth.currentUser?.email
-  console.log(signedInUser)
-
-  const handleSignOut = () => {
-    firebase
-      .auth()
-      .signOut()
-      .then(() => console.log('User Sign Out Success! ', signedInUser))
-      .catch(error => console.log('user cannot sign out: ', error))
-  }
-
+   const auth = firebase.auth()
+   const signedInUser = auth.currentUser?.email
+   console.log('this user is signed in: ', signedInUser)
   
+ 
+ const handleSignOut = () => {
+   firebase
+     .auth()
+     .signOut()
+     .then(() => console.log('User Sign Out Success! ', signedInUser))
+     .catch(error => console.log('user cannot sign out: ', error))
+ }
+ 
     return (
-      <>
-        <DrawerContentScrollView {...props}>
-        <DrawerItemList {...props}/>
-          <DrawerItem 
-          style={styles.signOutItem}
-          label='Sign Out'
-          onPress={() => handleSignOut()}
-          />
-          {/* <Text>Email: {auth.currentUser?.email}</Text> */}
-        </DrawerContentScrollView>
-      </>
-    )
-  }
+     <>
+       <DrawerContentScrollView {...props}>
+       <DrawerItemList {...props}/>
+         <DrawerItem
+         style={styles.signOutItem}
+         label='Sign Out'
+         onPress={() => handleSignOut()}
+         />
+       </DrawerContentScrollView>
+     </>
+   )
+ }
+

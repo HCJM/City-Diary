b6538ba83043d1f5aaa9484b3fbececbb05c05fa
mauilema <srjorgemaciel2002@gmail.com>
2022-01-20 21:32:26 -0500

    removed unneccessary fragments in the custom content file

--- src/screens/CustomDrawerContent/CustomDrawerContent.js
+++ src/screens/CustomDrawerContent/CustomDrawerContent.js
@@ -2,8 +2,6 @@ import * as React from 'react'
 import styles from "./styles"
 import { Image, Text, View, TouchableOpacity } from "react-native"
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer'
-// import firebase from 'firebase/compat/app'
-// import 'firebase/compat/auth'
 import { firebase } from '../../../firebase.js'
 import { useNavigation } from '@react-navigation/native'
  
@@ -14,7 +12,6 @@ import { useNavigation } from '@react-navigation/native'
 export function CustomDrawerContent(props) {
   const auth = firebase.auth()
   const signedInUser = auth.currentUser?.email
-  console.log('this user is signed in: ', signedInUser)
   
   const navigation = useNavigation()
 
@@ -22,21 +19,19 @@ export function CustomDrawerContent(props) {
     firebase
       .auth()
       .signOut()
-      .then(() => {console.log('User Sign Out Success! ', signedInUser), navigation.navigate('Landing Page')})
+      .then(() => navigation.navigate('Landing Page'))
       .catch(error => console.log('user cannot sign out: ', error))
   }
  
     return (
-     <>
        <DrawerContentScrollView {...props}>
        <DrawerItemList {...props}/>
          <DrawerItem
          style={styles.signOutItem}
-         label='Sign Out'
+         label='Log Out'
          onPress={() => handleSignOut()}
          />
        </DrawerContentScrollView>
-     </>
    )
  }
 

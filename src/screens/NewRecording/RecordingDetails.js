cd7804a4ca879e03185233e3cd677f762576e105
Chenel Morgan <chenel.morgan@gmail.com>
2022-01-22 13:51:28 -0500

    added comments

--- src/screens/NewRecording/RecordingDetails.js
+++ src/screens/NewRecording/RecordingDetails.js
@@ -4,30 +4,115 @@ import {
   StyleSheet,
   Text,
   View,
-  Dimensions,
+  SafeAreaView,
+  Image,
   TouchableOpacity,
   Pressable,
   Modal,
   TextInput,
 } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Audio } from 'expo-av'
+import { firebase } from '../../../firebase.js'
 
-export default function RecordingDetailsModal() {
-  const [modalVisible, setModalVisible] = useState(true)
+import * as Location from 'expo-location'
+
+export default function RecordingDetailsModal({ route, navigation }) {
+  const [modalVisible, setModalVisible] = useState(false)
   const [title, onChangeTitle] = React.useState('')
   const [description, onChangeDescription] = React.useState('')
-  // upload button both uploads and navigates to map
+  const { uid } = route.params
+
+  async function storeAudio() {
+    try {
+      // creating a blob
+      const blob = await new Promise((resolve, reject) => {
+        // opening a new request
+        const xhr = new XMLHttpRequest()
+        // once request is recieved
+        xhr.onload = () => {
+          try {
+            resolve(xhr.response)
+          } catch (error) {
+            console.error('error xhr.onload:', error)
+          }
+        }
+        xhr.onerror = (error) => {
+          console.error(error)
+          reject(new TypeError('Network request failed'))
+        }
+        xhr.responseType = 'blob'
+        xhr.open('GET', userRecording, true)
+        xhr.send(null)
+      })
+      if (blob != null) {
+        const uriParts = userRecording.split('.')
+        const fileType = uriParts[uriParts.length - 1]
+        firebase
+          .storage()
+          .ref()
+          .child(`${fileName}.${uid}.${fileType}`)
+          .put(blob, {
+            contentType: `audio/${fileType}`,
+          })
+          .then(() => {
+            console.log('Sent!')
+            sendToDatabase()
+          })
+          .catch((e) => console.log('error!:', e))
+      } else {
+        console.log('erroor with blob')
+      }
+    } catch (error) {
+      console.log('error!!:', error)
+    }
+  }
+
+  async function sendToDatabase() {
+    try {
+      let location = await Location.getCurrentPositionAsync({})
+
+      const uri = await firebase
+        .storage()
+        .ref()
+        .child(`${fileName}.${uid}.m4a`)
+        .getDownloadURL()
+
+      const instance = firebase.firestore().collection('audio')
+      instance.add({
+        title,
+        description,
+        isPrivate: false,
+        uploadedAt: new Date(),
+        userId: firebase.auth().currentUser.uid,
+        username: firebase.auth().currentUser.providerData[0].email,
+        downloadUrl: uri,
+        location: new firebase.firestore.GeoPoint(
+          location.coords.latitude,
+          location.coords.longitude
+        ),
+      })
+      console.log('Added!')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function onModalExit() {
+    onChangeTitle('')
+    onChangeDescription('')
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView>
+      {/* Modal Start */}
       <View style={styles.centeredView}>
         <Modal
           animationType="slide"
           transparent={true}
           visible={modalVisible}
-          onRequestClose={() => {
-            Alert.alert('Modal has been closed.')
-            setModalVisible(!modalVisible)
-          }}
+          // onRequestClose={() => {
+          //   setModalVisible(!modalVisible)
+          // }}
         >
           <View style={styles.centeredView}>
             <View style={styles.modalView}>
@@ -45,31 +130,48 @@ export default function RecordingDetailsModal() {
                 multiline={true}
                 value={description}
               />
-              <Pressable
-                style={[styles.button, styles.buttonClose]}
-                onPress={() => setModalVisible(!modalVisible)}
+              <TouchableOpacity
+                style={[styles.modalButton, styles.buttonClose]}
+                onPress={() => {
+                  setModalVisible(!modalVisible)
+                  onModalExit()
+                  storeAudio()
+                  // navigate to personal instead? easy fix
+                  navigation.navigate('Public Audio Map')
+                }}
               >
                 <Text style={styles.textStyle}>Upload</Text>
-              </Pressable>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.modalButton, styles.buttonClose]}
+                onPress={() => {
+                  setModalVisible(!modalVisible)
+                  onModalExit()
+                }}
+              >
+                <Text style={styles.textStyle}>Cancel</Text>
+              </TouchableOpacity>
             </View>
           </View>
         </Modal>
       </View>
+      {/* Modal End */}
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#A9A9A9',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'black',
+    fontSize: 17,
+    textAlign: 'center',
+    paddingTop: 30,
   },
   input: {
     height: 40,
@@ -77,6 +179,22 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     width: 200,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    alignSelf: 'center',
+    marginTop: 15,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'grey',
+    padding: 13,
+    paddingHorizontal: 20,
+    marginTop: 42,
+    marginHorizontal: 65,
+    width: 250,
   },
   // Modal start
   centeredView: {
@@ -100,7 +218,7 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5,
   },
-  button: {
+  modalButton: {
     borderRadius: 20,
     padding: 10,
     elevation: 2,
@@ -110,6 +228,7 @@ const styles = StyleSheet.create({
   },
   buttonClose: {
     backgroundColor: '#2196F3',
+    marginVertical: 15,
   },
   textStyle: {
     color: 'white',

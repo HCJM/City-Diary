c59572302918b8278f93ae280d97812b05ec9c5c
Chenel Morgan <chenel.morgan@gmail.com>
2022-01-21 20:27:35 -0500

    recording details modal renders properly

--- src/screens/PublicMapScreen/PublicMapScreen.js
+++ src/screens/PublicMapScreen/PublicMapScreen.js
@@ -14,7 +14,7 @@ import {
   Modal,
 } from 'react-native'
 import { Audio } from 'expo-av'
-// import styles from './styles'
+import styles from './styles'
 import * as Location from 'expo-location'
 
 const deltas = {
@@ -23,7 +23,6 @@ const deltas = {
 }
 
 export default function PublicMapScreen({ navigation }) {
-  const userId = firebase.auth().currentUser.uid
   const onRecordPress = () => {
     navigation.navigate('New Recording')
   }
@@ -208,84 +207,3 @@ playSound logic
     </View>
   )
 }
-
-export const styles = StyleSheet.create({
-  // Modal start
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: '#F194FF',
-  },
-  buttonClose: {
-    backgroundColor: '#2196F3',
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
-  },
-  // Modal end
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-  recordButton: {
-    position: 'absolute',
-    bottom: 50,
-    backgroundColor: '#DC143C',
-    marginLeft: 30,
-    marginRight: 30,
-    marginTop: 20,
-    height: 48,
-    width: 250,
-    borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonTitle: {
-    color: 'white',
-    fontSize: 22,
-    fontWeight: 'bold',
-  },
-  paragraph: {
-    fontSize: 16,
-    textAlign: 'center',
-    position: 'absolute',
-    top: 20,
-    backgroundColor: '#fff',
-  },
-})

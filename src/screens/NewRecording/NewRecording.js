6323b23611aee60af93e4a011e2855705eb5e52a
Chenel Morgan <chenel.morgan@gmail.com>
2022-01-21 19:52:08 -0500

    modular stylesheets

--- src/screens/NewRecording/NewRecording.js
+++ src/screens/NewRecording/NewRecording.js
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
 } from 'react-native'
 import { Audio } from 'expo-av'
+import styles from './styles'
 
 export default function NewRecording({ navigation }) {
   const [recording, setRecording] = React.useState()
@@ -80,34 +81,3 @@ export default function NewRecording({ navigation }) {
     </SafeAreaView>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#A9A9A9',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'black',
-    fontSize: 17,
-    textAlign: 'center',
-    paddingTop: 30,
-  },
-  image: {
-    width: 150,
-    height: 150,
-    alignSelf: 'center',
-    marginTop: 15,
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'grey',
-    padding: 13,
-    paddingHorizontal: 20,
-    marginTop: 42,
-    marginHorizontal: 65,
-    width: 250,
-  },
-})

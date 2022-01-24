0e30c8e239b5e468d858d778fd2c4788ddf3847e
Heather White <miss.pomykal@gmail.com>
2022-01-23 22:10:30 -0500

    CLEAN: landing screen dir

--- src/screens/LandingScreen/styles.js
+++ src/screens/LandingScreen/styles.js
@@ -1,55 +1,46 @@
-import { StyleSheet } from "react-native"
- 
-// export default StyleSheet.create({
-//    container: {
-//        flex: 1,
-//        alignItems: "center"
-//    }
-// })
-
-
+import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    logo: {
-      width: 200,
-      height: 200,
-    },
-    exploreButton: {
-      backgroundColor: "#788eec",
-      marginLeft: 30,
-      marginRight: 30,
-      marginTop: 70,
-      height: 48,
-      width:330,
-      borderRadius: 5,
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    exploreButtonTitle: {
-      color: "white",
-      fontSize: 17,
-      fontWeight: "bold",
-    },
-    signUpLoginButton: {
-      backgroundColor: "#b047ed",
-      marginLeft: 30,
-      marginRight: 30,
-      marginTop: 70,
-      height: 48,
-      width: 330,
-      borderRadius: 5,
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    signUpLoginButtonTitle: {
-      color: "white",
-      fontSize: 16,
-      fontWeight: "bold",
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+  exploreButton: {
+    backgroundColor: '#788eec',
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 70,
+    height: 48,
+    width: 330,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  exploreButtonTitle: {
+    color: 'white',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+  signUpLoginButton: {
+    backgroundColor: '#b047ed',
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 70,
+    height: 48,
+    width: 330,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  signUpLoginButtonTitle: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+})

484c7274c1e958425d3f8c60ef73e11b5fa50394
mauilema <srjorgemaciel2002@gmail.com>
2022-01-24 17:18:21 -0500

    added jsx and styling to the profile screen view

--- src/screens/ProfileScreen/ProfileScreen.js
+++ src/screens/ProfileScreen/ProfileScreen.js
@@ -1,13 +1,56 @@
-import { Text, View } from 'react-native'
+import { View, SafeAreaView } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import { Caption, Text, Title, TouchableRipple } from 'react-native-paper'
 import styles from './styles.js'
 
 
 
 export default function ProfileScreen ({ navigation }) {
     return (
-        <View style={styles.hello}>
-            <Text>There Should Be User First Name Here!</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView>
+                <View style={styles.userInfoSection}>
+                    <View style={{flexDirection: 'row', marginTop: 15}}>
+                    <View style={{marginLeft: 20}}>
+                        <Title style={[styles.title, {
+                        marginTop:15,
+                        marginBottom: 5,
+                        }]}>Hello Jose!</Title>
+                        <Caption style={styles.caption}>@mauilema</Caption>
+                    </View>
+                    </View>
+                </View>
+
+                <View style={styles.userInfoSection}>
+                    <View style={styles.row}>
+                    <Text style={{color:"#777777", marginLeft: 20}}>Brooklyn, NY</Text>
+                    </View>
+                </View>
+
+                <View style={styles.menuWrapper}>
+                    <TouchableRipple onPress={() => {}}>
+                    <View style={styles.menuItem}>
+                        <Text style={styles.menuItemText}>My Latest Entry 1-24-22</Text>
+                    </View>
+                    </TouchableRipple>
+                    <TouchableRipple onPress={() => {}}>
+                    <View style={styles.menuItem}>
+                        <Text style={styles.menuItemText}>My Latest Entry 1-23-22</Text>
+                    </View>
+                    </TouchableRipple>
+                    <TouchableRipple onPress={() => {}}>
+                    <View style={styles.menuItem}>
+                        <Text style={styles.menuItemText}>My Latest Entry 1-22-22</Text>
+                    </View>
+                    </TouchableRipple>
+                    <TouchableRipple onPress={() => {}}>
+                    <View style={styles.menuItem}>
+                        <Text style={styles.menuItemText}>My Latest Entry 1-21-22</Text>
+                    </View>
+                    </TouchableRipple>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
--- src/screens/ProfileScreen/styles.js
+++ src/screens/ProfileScreen/styles.js
@@ -1,12 +1,41 @@
 import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
-    hello: {
+    container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        },
+      },
+      userInfoSection: {
+        paddingHorizontal: 30,
+        marginBottom: 25,
+      },
+      title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+      },
+      caption: {
+        fontSize: 14,
+        lineHeight: 14,
+        fontWeight: '500',
+      },
+      row: {
+        flexDirection: 'row',
+        marginBottom: 10,
+      },
+      menuWrapper: {
+        marginTop: 10,
+      },
+      menuItem: {
+        flexDirection: 'row',
+        paddingVertical: 15,
+        paddingHorizontal: 30,
+      },
+      menuItemText: {
+        color: '#777777',
+        marginLeft: 20,
+        fontWeight: '600',
+        fontSize: 16,
+        lineHeight: 26,
+      },
 })
 
 

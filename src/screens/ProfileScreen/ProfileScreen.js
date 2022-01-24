ffd8035c80d223e9035fb77e84723b980ef9787a
mauilema <srjorgemaciel2002@gmail.com>
2022-01-24 17:29:10 -0500

    modularized the styles from the profile screen view for consistency, added a console log to the touchable ripple to test functionality

--- src/screens/ProfileScreen/ProfileScreen.js
+++ src/screens/ProfileScreen/ProfileScreen.js
@@ -10,12 +10,9 @@ export default function ProfileScreen ({ navigation }) {
         <SafeAreaView style={styles.container}>
             <ScrollView>
                 <View style={styles.userInfoSection}>
-                    <View style={{flexDirection: 'row', marginTop: 15}}>
-                    <View style={{marginLeft: 20}}>
-                        <Title style={[styles.title, {
-                        marginTop:15,
-                        marginBottom: 5,
-                        }]}>Hello Jose!</Title>
+                    <View style={styles.nameUserNameView}>
+                    <View style={styles.nameUserName}>
+                        <Title style={styles.title}>Hello Jose!</Title>
                         <Caption style={styles.caption}>@mauilema</Caption>
                     </View>
                     </View>
@@ -23,27 +20,27 @@ export default function ProfileScreen ({ navigation }) {
 
                 <View style={styles.userInfoSection}>
                     <View style={styles.row}>
-                    <Text style={{color:"#777777", marginLeft: 20}}>Brooklyn, NY</Text>
+                    <Text style={styles.locationText}>Brooklyn, NY</Text>
                     </View>
                 </View>
 
                 <View style={styles.menuWrapper}>
-                    <TouchableRipple onPress={() => {}}>
+                    <TouchableRipple onPress={() => console.log('hello friend')}>
                     <View style={styles.menuItem}>
                         <Text style={styles.menuItemText}>My Latest Entry 1-24-22</Text>
                     </View>
                     </TouchableRipple>
-                    <TouchableRipple onPress={() => {}}>
+                    <TouchableRipple onPress={() => console.log('hello friend')}>
                     <View style={styles.menuItem}>
                         <Text style={styles.menuItemText}>My Latest Entry 1-23-22</Text>
                     </View>
                     </TouchableRipple>
-                    <TouchableRipple onPress={() => {}}>
+                    <TouchableRipple onPress={() => console.log('hello friend')}>
                     <View style={styles.menuItem}>
                         <Text style={styles.menuItemText}>My Latest Entry 1-22-22</Text>
                     </View>
                     </TouchableRipple>
-                    <TouchableRipple onPress={() => {}}>
+                    <TouchableRipple onPress={() => console.log('hello friend')}>
                     <View style={styles.menuItem}>
                         <Text style={styles.menuItemText}>My Latest Entry 1-21-22</Text>
                     </View>

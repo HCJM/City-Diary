2275bac0e64167b613baeee5890e1684957d2f4d
Chenel Morgan <chenel.morgan@gmail.com>
2022-02-09 21:36:19 -0500

    fixed bundling issue

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import { registerRootComponent } from 'expo'
 
 import App from './App'
 
+import { LogBox } from 'react-native'
+LogBox.ignoreLogs(['Setting a timer'])
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
